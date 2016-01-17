17de571be36512f674f994af5943bea4e7bb977c
Carl Harris <elgoogemail2007@gmail.com>
2016-01-17 05:15:26 -0500

    removed class, seemed like overkill

--- bookscraper.py
+++ bookscraper.py
@@ -6,82 +6,87 @@ import urlparse
 from bs4 import BeautifulSoup as BS
 from pprint import pprint as pp
 
-class BookScraper(object):
+__all__ = ['scrapebook']
 
-    URL = 'http://catalog.kentonlibrary.org/eg/opac/results'
+KENTON_URL = 'http://catalog.kentonlibrary.org/eg/opac/results'
 
-    def __init__(self, url=None):
-        self.url = url or self.URL
+def _add_book_data(book, soup, url):
+    """
+    Modify book in-place with details from soup
+    """
+    # title
+    title = book['title'] = soup.find(id='rdetail_title')
+    if title:
+        book['title'] = title.string
 
-    def _add_book_data(self, book, soup):
-        """
-        modify book in-place with details from soup
-        """
-        # title
-        title = book['title'] = soup.find(id='rdetail_title')
-        if title:
-            book['title'] = title.string
+    # authors
+    authors = book['authors'] = []
+    for tag in soup.select('.rdetail-author-div'):
+        authors.append(''.join(tag.stripped_strings))
 
-        # authors
-        authors = book['authors'] = []
-        for tag in soup.select('.rdetail-author-div'):
-            authors.append(''.join(tag.stripped_strings))
+    def detailgetter(tag):
+        key = tag.select_one('.rdetail_label')
+        if key:
+            key = key.string.rstrip(':')
 
-        def detailgetter(tag):
-            key = tag.select_one('.rdetail_label')
-            if key:
-                key = key.string.rstrip(':')
+        value = tag.select_one('.rdetail_value')
+        if value:
+            value = value.string
 
-            value = tag.select_one('.rdetail_value')
-            if value:
-                value = value.string
+        return (key, value)
 
-            return (key, value)
+    # book details
+    details = book['details'] = {}
+    for tag in soup.find(id='rdetail_record_details').find('ul').find_all('li'):
+        key, val = detailgetter(tag)
+        if not (key or val):
+            continue
 
-        # book details
-        details = book['details'] = {}
-        for tag in soup.find(id='rdetail_record_details').find('ul').find_all('li'):
-            key, val = detailgetter(tag)
-            if not (key or val):
-                continue
+        if key in details:
+            try:
+                details.append(val)
+            except AttributeError:
+                details[key] = [ details[key], val ]
+        else:
+            details[key] = val
 
-            if key in details:
-                try:
-                    details.append(val)
-                except AttributeError:
-                    details[key] = [ details[key], val ]
+    # images
+    imagetag = soup.find(id='rdetail_image')
+    if imagetag:
+        book['image_medium'] = urlparse.urljoin(url, imagetag['src'])
+        book['image_large'] = urlparse.urljoin(url, imagetag.parent['href'])
+
+def scrapebook(isbn, url=None):
+    """
+    Scrape book information from the Kenton County Library Catalog website.
+
+    Returns a dictionary of data scraped. An empty dictionary if not found.
+    """
+    if url is None:
+        url = KENTON_URL
+
+    book = {}
+
+    result = requests.get(url, params=dict(query=isbn, qtype='keyword'))
+    if result:
+        soup = BS(result.text, 'lxml')
+        _add_book_data(book, soup, url)
+
+        if 'details' in book and 'ISBN' in book['details']:
+            # check that one of the ISBNs equal the ISBN we searched for
+            isbns = book['details']['ISBN']
+
+            # wrap in a list if string
+            if isinstance(isbns, basestring):
+                isbns = [isbns]
+
+            for value in isbns:
+                if isbn in value.split():
+                    break
             else:
-                details[key] = val
+                book = {}
 
-        # images
-        imagetag = soup.find(id='rdetail_image')
-        if imagetag:
-            book['image_medium'] = urlparse.urljoin(self.url, imagetag['src'])
-            book['image_large'] = urlparse.urljoin(self.url, imagetag.parent['href'])
-
-    def scrape(self, isbn):
-        book = {}
-
-        result = requests.get(self.url, params=dict(query=isbn, qtype='keyword'))
-        if result:
-            soup = BS(result.text, 'lxml')
-            self._add_book_data(book, soup)
-
-            if 'details' in book and 'ISBN' in book['details']:
-                # check that one of the ISBNs equal the ISBN we searched for
-                isbns = book['details']['ISBN']
-
-                # wrap in a list if string
-                if isinstance(isbns, basestring):
-                    isbns = [isbns]
-
-                for value in isbns:
-                    if isbn in value.split():
-                        break
-                else:
-                    book = {}
-
-        return book
+    return book
 
 
 def ArgParser():
@@ -93,8 +98,7 @@ def ArgParser():
 def main():
     parser = ArgParser()
     args = parser.parse_args()
-    scraper = BookScraper()
-    book = scraper.scrape(args.isbn)
+    book = scrapebook(args.isbn)
     if book:
         pp(book)
 
